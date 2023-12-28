0adc2d18fb2be07be2bc051680ea6498dca9c07b
mazen ahmed <155106298+mazenahmed12334@users.noreply.github.com>
2023-12-28 23:14:12 +0200

    Add files via upload

---
+++ project java/LoginAndSignUpAPPLICATION.java
@@ -0,0 +1,16 @@
+
+import loginandsignupapplication.Login;
+
+
+public class LoginAndSignUpAPPLICATION {
+
+     
+    public static void main(String[] args) {
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+        // TODO code application logic here
+    }
+    
+}
